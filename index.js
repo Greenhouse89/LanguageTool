05b49a31a569450d74f9b51c4535d0f401bb10f0
Greenhouse89 <greenbrandon07@yahoo.com>
2021-06-23 13:21:21 -0400

    added get messages

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
 const app = express();
 const pgp = require("pg-promise")();
 
+
 const db = pgp('postgres://brandon:Quattro.c7@localhost:5432/LearnTool');
 const server = http.createServer(app);
+
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}`);
@@ -26,9 +28,12 @@ app.get('/proficiencies', (req, res) =>{
 app.get('/languages', (req, res) =>{
     var allLang = db.any("SELECT * from LANGUAGES") .then((allLang) => res.send(allLang));
 });
+app.get('/messages', (req, res) =>{
+    var allMessages = db.any("SELECT * from MESSAGES") .then((allMessages) => res.send(allMessages));
+});
+
 
 app.get('/users',(req, res) =>{
     var userInfo= db.any("SELECT * from USERS") .then((userInfo) => res.send(userInfo));
     //console.log(userInfo);
-    
 });
