7b31c111273a916bbf7c87a93ecbcc010448860e
Greenhouse89 <greenbrandon07@yahoo.com>
2021-06-23 11:25:50 -0400

    adding files that are needed

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const express = require('express');
+const { userInfo } = require('os');
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
+const app = express();
+const pgp = require("pg-promise")();
+
+const db = pgp('postgres://brandon:Quattro.c7@localhost:5432/LearnTool');
+const server = http.createServer(app);
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+})
+
+app.get('/', (req, res) =>{
+    res.send('Welcome to Learn App');
+})
+app.get('/proficiencies', (req, res) =>{
+    var langProf = db.any('SELECT * FROM PROFICIENCIES') .then((langProf) => res.send(langProf));
+})
+
+
+app.get('/users',(req, res) =>{
+    var userInfo= db.any("SELECT * from USERS") .then((userInfo) => res.send(userInfo));
+    //console.log(userInfo);
+    
+})
---
+++ seed.sql
@@ -0,0 +1,9 @@
+insert into users
+    (email, firstname, lastname)
+values
+    ( 'alice@email.com','Alice', 'Bryant'),
+    ( 'bob@email.com','Bob','Javobs')
+    ('cho@email.com','Cho', 'Nguyen'),
+    ( 'daryl@email.com','Daryl','Hopkins'),
+    ( 'emmy@email.com','Emmy','Martin')
+;
